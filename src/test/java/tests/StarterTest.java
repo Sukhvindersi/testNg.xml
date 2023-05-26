5e525d70d4fa05aad1de7ac3ebdebad7d346747b
amandiwan <amandiwan7@gmail.com>
2023-05-26 11:51:43 +0530

    Basic testNG.xml framework to run single test in three browsers parallelly

---
+++ src/test/java/tests/StarterTest.java
@@ -0,0 +1,62 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+
+
+public class StarterTest {
+	SoftAssert softassert=new SoftAssert();
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setup() {
+		
+		
+//		driver=new ChromeDriver();
+		
+	}
+	
+//	@Test
+//	public void testOneFirefox() {
+//		driver=new FirefoxDriver();
+//		driver.get("https://www.google.com/");
+//		String abc="abc";
+//		String xyz="abc";
+//		softassert.assertEquals(abc, xyz);
+//	}
+//	@Test()
+//	public void testOneEdge() {
+//		driver=new EdgeDriver();
+//		driver.get("https://www.google.com/");
+//		String pqr="xyz";
+//		String mnp="xyz";
+//		softassert.assertEquals(pqr, mnp);
+//	}
+	
+	@Test(testName="firefoxBrowser", timeOut=10000)
+	@Parameters("browser")
+	public void dynamicBrowser(String browser) throws InterruptedException {
+		if(browser.equalsIgnoreCase("chrome")) {
+			driver=new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("Firefox")) {
+			driver=new FirefoxDriver();
+		}
+		if(browser.equalsIgnoreCase("edge")) {
+			driver=new EdgeDriver();
+		}
+		else {
+			System.out.println("\u001B[31m"+"No browser value passed in the 'src/test/java/testng.xml' file!");//The "\u001B[31m" is an ANSI escape code for setting the message color red
+		}
+		Thread.sleep(15000);
+	}
+	
+}
